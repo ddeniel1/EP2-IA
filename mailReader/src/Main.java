e303e75a4faf16f826d60116c33d0c9e97b6b373
ddeniel1 <ddeniel1@yahoo.com.br>
2019-05-27 23:57:24 -0300

    mods importantes, leitura de emails e adicao no hash

--- mailReader/src/Main.java
+++ mailReader/src/Main.java
@@ -1,10 +1,25 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.util.ArrayList;
 
 public class Main {
-    public static void main(String[] args){
+    public static void main(String[] args) throws java.io.FileNotFoundException, java.io.IOException{
         FileReader arq = new FileReader("./mailReader/emails.csv");
         BufferedReader buff = new BufferedReader(arq);
-
+        ArrayList<Mail> emails = new ArrayList<Mail>();
+        buff.readLine();
+        Mail novo;
+        while(buff.ready()){
+            System.out.println("Aqui");
+            novo = new Mail();
+            String aux = buff.readLine();
+            do {
+                aux = aux+"\n"+buff.readLine();
+            }while (aux.charAt(aux.length()-1)!='"');
+            System.out.println(aux);
+            novo.addMail(aux);
+            emails.add(novo);
+        }
+        System.out.println(emails.get(0).toString());
     }
 }
--- mailReader/src/mail.java
+++ mailReader/src/mail.java
@@ -1,20 +1,40 @@
 import java.util.Collection;
 import java.util.HashMap;
+import java.util.Set;
 
-public class mail {
-    private HashMap<String,Integer> palavras;
+public class Mail {
+    private HashMap<String,Integer> palavras = new HashMap<String, Integer>();
 
-    public mail(){
+    public Mail(){
         palavras.clear();
-        palavras.
     }
+
+    @Override public String toString(){
+        String resposta = "";
+        String[] keys = (String[]) palavras.keySet().toArray();
+        for (int i = 0; i < keys.length; i++) {
+            resposta = resposta + keys[i] +" : " + palavras.get(keys[i]) + "\n";
+        }
+        return resposta;
+    }
+
     public void addPalavra(String palavra){
         if(palavras.containsKey(palavra)){
+
             int atual = palavras.get(palavra);
             palavras.replace(palavra,atual+1);
             return;
         }
         palavras.put(palavra,1);
+    }
+    public void addMail(String email){
+
+        String[] emailTemp = email.split(" ");
+        for (int i = 0; i < emailTemp.length; i++) {
+            System.out.println(emailTemp[i]);
+            addPalavra(emailTemp[i]);
+
+        }
     }
     public int tamVocabulario(){
         return palavras.size();
